7de52cc758af8f395002232cb8afc873afab5b12
PriyaGondaliya <priyagondaliya444@gmail.com>
2022-03-14 17:20:02 -0400

    Customize Cake UI Done

---
+++ cakeApp/Controller/Utilities.swift
@@ -0,0 +1,29 @@
+//
+//  Utilities.swift
+//  cakeApp
+//
+//  Created by Mayur Khambhadiya on 2022-03-14.
+//
+
+import UIKit
+
+class Utilities: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ cakeAppAdmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cakeAppAdmin
+//
+//  Created by Mayur Khambhadiya on 2022-03-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
