c694815dfb1a066e68c800a3ef66df5df81f20e4
krinesh2598 <krineshpatel6123@gmail.com>
2022-04-05 22:08:38 -0400

    Shipping UI Done

--- cakeApp/Model/CartProduct.swift
+++ cakeApp/Model/CartProduct.swift
@@ -1,8 +1,18 @@
-//
-//  CartProduct.swift
-//  cakeApp
-//
-//  Created by Mayur Khambhadiya on 2022-04-05.
-//
-
 import Foundation
+
+struct CartProduct{
+    var name: String
+    var id: String
+    var image: String
+    var desc: String
+    var price: String
+    var qty: Int
+    var type: String
+    var design: String
+    var size: String
+    var shape: String
+    var flavour: String
+    var note: String
+    var customType: String
+    
+}
--- cakeApp/View/CartItemCell.swift
+++ cakeApp/View/CartItemCell.swift
@@ -1,23 +1,49 @@
-//
-//  CartItemCell.swift
-//  cakeApp
-//
-//  Created by Mayur Khambhadiya on 2022-04-05.
-//
-
 import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+
+protocol CartItemDelegate: AnyObject {
+    
+    func removeItem(cartProduct: CartProduct)
+}
 
 class CartItemCell: UITableViewCell {
 
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productQty: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var removeProductButton: UIButton!
+    var cartItems = [CartProduct]()
+
+    let db =  Firestore.firestore()
+
+    private var item: CartProduct!
+    weak var delegate : CartItemDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    func configureCell(cartProduct: CartProduct, delegate: CartItemDelegate) {
+        
+        self.delegate = delegate
+        self.item = cartProduct
+        
+        productName.text = cartProduct.name
+        productPrice.text = "Price: $" + String(cartProduct.price)
+        productQty.text = "Quantity: " + String(cartProduct.qty)
+        productImage.image = UIImage(named: cartProduct.image)
+       
+    }
+    
+    @IBAction func removeButtonClicked(_ sender: Any) {
+        
+        delegate?.removeItem(cartProduct: item)
 
-        // Configure the view for the selected state
     }
     
 }
+
