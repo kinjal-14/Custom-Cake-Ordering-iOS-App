d70616cca59d1c4417e830299016ed29c400a83b
PriyaGondaliya <priyagondaliya444@gmail.com>
2022-04-05 21:38:25 -0400

    Cutomize Cake Cell for Customize Cake UI

---
+++ cakeApp/Constant.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+    
+struct Storyboard {
+    
+    static let homeViewController = "MainVC"
+}
+    
+struct Identifiers {
+    
+    static let CustomizeCakeCell = "CustomizeCakeCell"
+    
+}
+
+struct Segues {
+    static let ToProducts = "goToCakeDetails"
+    
+}
---
+++ cakeApp/View/CustomizeCakeCell.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class CustomizeCakeCell: UICollectionViewCell {
+
+    @IBOutlet weak var cakeTypeImage: UIImageView!
+    @IBOutlet weak var cakeTypeName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        cakeTypeImage.layer.cornerRadius = 5
+
+    }
+    
+    func configureCell(customizeCake: CustomizeCake) {
+        
+        cakeTypeName.text = customizeCake.name
+        cakeTypeImage.image = UIImage(named:customizeCake.image)
+            
+    }
+    func getData(customizeCake: CustomizeCake) -> (String){
+        
+        return (customizeCake.name)
+        return (customizeCake.image)
+    }
+
+}
