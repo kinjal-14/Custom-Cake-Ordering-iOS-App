5f2b217c00f5ef867f20bd3cdf0f11fde24813ab
kinjal-14 <kunjadiyakinjal1407@gmail.com>
2022-04-19 13:10:34 -0400

    Constants Updated

--- cakeApp/Controller/Constants.swift
+++ cakeApp/Controller/Constants.swift
@@ -1,8 +1,24 @@
-//
-//  Constants.swift
-//  cakeApp
-//
-//  Created by Mayur Khambhadiya on 2022-03-14.
-//
-
 import Foundation
+import UIKit
+    
+struct Storyboard {
+    
+    static let homeViewController = "MainVC"
+}
+    
+struct Identifiers {
+    static let ProductCell = "ProductCell"
+    static let ProductDetailsCell = "ProductDetailsCell"
+    static let CustomizeCakeCell = "CustomizeCakeCell"
+    static let CartProductCell = "CartProductCell"
+    static let CartItemCell = "CartItemCell"
+    static let OrderItemCell = "OrderItemCell"
+    static let OrderItemProcessingCell = "OrderItemProcessingCell"
+    static let OrderDetailCell = "OrderDetailCell"
+    
+}
+
+struct Segues {
+    static let ToProducts = "goToCakeDetails"
+    
+}
